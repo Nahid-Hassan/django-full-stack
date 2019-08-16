99c9ab7d8fb48df3b0ed887d997cb497d3ef4943
Nahid-Hassan <nahid.cseru@gmail.com>
2019-08-16 21:16:44 +0600

    learn django template basics

--- Django_Level_1/first_project/first_app/views.py
+++ Django_Level_1/first_project/first_app/views.py
@@ -7,7 +7,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    my_dict = {
+        #key: value
+        'insert_me': 'Hello I am from views.py'
+    }
+
+    return render(request, 'first_app/index.html', context=my_dict)
+    
     #return HttpResponse(html)
-    return HttpResponse('First Django Web Application.')
+    #return HttpResponse('First Django Web Application.')
 
 
