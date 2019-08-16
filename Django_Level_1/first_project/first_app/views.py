f70321e98b160cbbb3a0b80b8212a4c23d4d9d5c
Nahid-Hassan <nahid.cseru@gmail.com>
2019-08-16 19:03:48 +0600

    Maping urls

--- Django_Level_1/first_project/first_app/views.py
+++ Django_Level_1/first_project/first_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#python file read
+# html = open(
+#     '/media/nahid/New Volume/Web-Developer/Full-Stack-Web-Developer/Capstone_Project_One/project.html', 'r')
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Now you learn Basic urls')
+    #return HttpResponse(html)
+    return HttpResponse('First Django Web Application.')
 
 
