c75e48f524f93c99c116e922434395e6034bae3c
Nahid-Hassan <nahid.cseru@gmail.com>
2019-08-17 09:07:16 +0600

    setting up first project

---
+++ Django_Level_2/first_project/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    my_dict = {
+        'insert_content': 'Hello, I am from first_app/views.py file.'
+    }
+
+    return render(request, 'first_app/index.html', context=my_dict)
