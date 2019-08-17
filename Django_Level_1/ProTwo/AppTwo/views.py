36ba2743ad7636832d9e543dbd08d27dfd730e31
Nahid-Hassan <nahid.cseru@gmail.com>
2019-08-17 08:23:32 +0600

    Inject static files using django template

--- Django_Level_1/ProTwo/AppTwo/views.py
+++ Django_Level_1/ProTwo/AppTwo/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-html = '<em>My Second App</em>'
+html = '<em>Now I am in views.py</em>'
 
 def index(request):
     return HttpResponse(html)
+
+
+def help(request):
+    help_dict = {
+        'help_insert':'Help Page'
+    }
+    
+    return render(request, 'index.html', context=help_dict)
